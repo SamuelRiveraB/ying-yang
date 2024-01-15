fc21ae5cac0624f55bab85c6d7cab6daf78e5bb1
Samuel <samuelrivba@gmail.com>
2024-01-14 21:57:31 -0500

    Add Local Storage and clean up

--- script.js
+++ script.js
@@ -6,36 +6,40 @@ const image2 = document.getElementById('image2')
 const image3 = document.getElementById('image3')
 const textBox = document.getElementById('text-box')
 
-function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)'
-    toggleIcon.children[0].textContent = 'Dark Mode'
-    toggleIcon.children[1].classList.remove('fa-sun')
-    toggleIcon.children[1].classList.add('fa-moon')
-    image1.src = 'img/undraw_code_inspection_dark.svg'
-    image2.src = 'img/undraw_feeling_proud_dark.svg'
-    image3.src = 'img/undraw_conceptual_idea_dark.svg'
+function imageMode(color) {
+    image1.src = `img/undraw_code_inspection_${color}.svg`
+    image2.src = `img/undraw_feeling_proud_${color}.svg`
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`
 }
 
-function lightMode() {
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)'
-    toggleIcon.children[0].textContent = 'Light Mode'
-    toggleIcon.children[1].classList.remove('fa-moon')
-    toggleIcon.children[1].classList.add('fa-sun')
-    image1.src = 'img/undraw_code_inspection_light.svg'
-    image2.src = 'img/undraw_feeling_proud_light.svg'
-    image3.src = 'img/undraw_conceptual_idea_light.svg'
-}
+function toggleDarkLight(isDark) {
+    nav.style.backgroundColor = isDark? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)'
+    textBox.style.backgroundColor = isDark? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)'
+    toggleIcon.children[0].textContent = isDark? 'Dark Mode' : 'Light Mode'
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
+    isDark ? imageMode('dark') : imageMode('light')
+} 
 
 function switchTheme(event) {
     if(event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
-        darkMode()
+        localStorage.setItem('theme', 'dark')
+        toggleDarkLight(true)
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
-        lightMode()
+        localStorage.setItem('theme', 'light')
+        toggleDarkLight(false)
     }
 }
 
 toggleSwitch.addEventListener('change', switchTheme)
+
+const currentTheme = localStorage.getItem('theme')
+if(currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme)
+
+    if(currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode()
+    }
+}
